5691fb0e3c3246f957cc96e9d11ba8170e5d6a2d
Troush <troush69@gmail.com>
2017-07-12 15:42:31 +0300

    make credo happy

--- lib/report/release_tasks.ex
+++ lib/report/release_tasks.ex
@@ -7,6 +7,7 @@ defmodule Report.ReleaseTasks do
       report/bin/report command Elixir.Report.ReleaseTasks migrate!
   """
   alias Ecto.Migrator
+  alias Ecto.Migration.Runner
 
   @start_apps [
     :logger,
@@ -67,23 +68,23 @@ defmodule Report.ReleaseTasks do
 
   def setup_pg_logical! do
     pg_logical = Confex.get(:report_api, :pg_logical_node)
-    Ecto.Migration.Runner.execute "SELECT pglogical.create_node(
+    Runner.execute "SELECT pglogical.create_node(
     node_name := ‘subscriber’,
     dsn := ‘#{pg_logical[:dsn]}’);"
 
-    Ecto.Migration.Runner.execute "SELECT pglogical.create_subscription(
+    Runner.execute "SELECT pglogical.create_subscription(
     subscription_name := ‘subscription_mpi’,
     provider_dsn := ‘#{pg_logical[:mpi_dsn]}’);"
 
-    Ecto.Migration.Runner.execute "SELECT pglogical.create_subscription(
+    Runner.execute "SELECT pglogical.create_subscription(
     subscription_name := ‘subscription_prm’,
     provider_dsn := ‘#{pg_logical[:prm_dsn]}’);"
 
-    Ecto.Migration.Runner.execute "SELECT pglogical.create_subscription(
+    Runner.execute "SELECT pglogical.create_subscription(
     subscription_name := ‘subscription_uaddresses’,
     provider_dsn := ‘#{pg_logical[:uaddresses_dsn]}’);"
 
-    Ecto.Migration.Runner.execute "SELECT pglogical.create_subscription(
+    Runner.execute "SELECT pglogical.create_subscription(
     subscription_name := ‘subscription_ops’,
     provider_dsn := ‘#{pg_logical[:ops_dsn]}’);"
   end
