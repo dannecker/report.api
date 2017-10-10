73ab66bd6ed6a73d60dc7dfe74eb6e9d235fd3d7
Alex Kovalevych <alexkovalevych@gmail.com>
2017-10-10 17:33:00 +0300

    remove extra party fields from replica

--- lib/report/replica/schemas/party.ex
+++ lib/report/replica/schemas/party.ex
@@ -9,14 +9,6 @@ defmodule Report.Replica.Party do
     field :first_name, :string
     field :last_name, :string
     field :second_name, :string
-    field :birth_date, :date
-    field :gender, :string
-    field :tax_id, :string
-    field :inserted_by, Ecto.UUID
-    field :updated_by, Ecto.UUID
-
-    embeds_many :phones, Report.Replica.Meta.Phone, on_replace: :delete
-    embeds_many :documents, Report.Replica.Meta.Document, on_replace: :delete
 
     has_many :users, Report.Replica.PartyUser, foreign_key: :party_id
 
